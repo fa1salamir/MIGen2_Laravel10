7e8d44a9033cb7b357f5d06cbc290f92972f2563
unknown <faisalamir5pt@gmail.com>
2024-04-16 15:26:42 +0700

    First Commit

---
+++ database/migrations/2024_03_18_080537_create_detail_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_user', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('type_user_id');
+            $table->string('contact')->nullable();
+            $table->longtext('address')->nullable();
+            $table->longtext('photo')->nullable();
+            $table->enum('gender', [1,2])->nullable();
+            $table->timestamps();
+            $table->softdeletes();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_user');
+    }
+};
